46176825e4b29b4caeaeb80b76ba7b42842286e8
Lior Rabinovich <lior.rabinovich@gthsoftware.com>
2019-01-07 11:52:06 +0200

    Added style

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -6,29 +6,54 @@ class Users extends Component {
     this.props.getUsers(this.pageNumber);
   }
   getUsersDOM() {
-    let usersDom = this.props.users.data.map((value, index) => (
-      <li key={value.id}>
-        <Link to={'/user/' + value.id}><strong>{value.id}</strong> {value.first_name} {value.last_name}</Link>
-      </li>
+    let usersItems = this.props.users.data.map((value, index) => (
+      <tr className="pointer" key={value.id} onClick={ () => this.handleClickUser(value.id) }>
+        <th>{value.id}</th>
+        <td>{value.first_name}</td>
+        <td>{value.last_name}</td>
+      </tr>
     ));
-    return (<ul className="list">{usersDom}</ul>);
+    return (
+      <div className="table">
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {usersItems}
+          </tbody>
+        </table>
+      </div>
+    );
   }
   getPagesDOM() {
     if (this.props.users.total_pages !== null) {
-      let pages = [];
+      let pagesItems = [];
       for (let pageNumber = 1; pageNumber < this.props.users.total_pages; pageNumber++) {
-        pages.push(
-          <Link to={'/users/' + pageNumber} key={pageNumber} onClick={ () => this.handleClickPage(pageNumber) }>{pageNumber}</Link>
+        let className = 'button';
+        if(this.props.users.page === pageNumber){
+          className += ' active';
+        }
+        pagesItems.push(
+          <button className={className} key={pageNumber} onClick={ () => this.handleClickPage(pageNumber) }>{pageNumber}</button>
         );
       }
-      return (<div className="pages">{pages}</div>);
+      return (<div className="group-button">{pagesItems}</div>);
     }
     return;
   }
   handleClickPage = (pageNumber) => {
     if(this.props.users.page !== pageNumber){
+      this.props.history.push(`/users/${pageNumber}`);
       this.props.getUsers(pageNumber);
     }
+  }
+  handleClickUser = (userID) => {
+    this.props.history.push(`/user/${userID}`);
   }
   get pageNumber() {
     let isPageNumber = typeof this.props.match.params.pageNumber !== 'undefined';
